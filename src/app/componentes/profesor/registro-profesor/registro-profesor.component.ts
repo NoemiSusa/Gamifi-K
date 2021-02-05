36829e6dce18af8f280867b6192c2790dccbd4d5
NoemiSusa <noemisusa@gmail.com>
2021-02-05 20:54:55 +0100

    formulario reactivo falla pero funciona validacion correo

--- src/app/componentes/profesor/registro-profesor/registro-profesor.component.ts
+++ src/app/componentes/profesor/registro-profesor/registro-profesor.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Profesor } from 'src/app/models/profesor.model';
-import { validarContrasenya } from '../../../models/validarContrasenya'
+import { validarContrasenya } from './validarContrasenya'
 import Swal from 'sweetalert2';
 
 
@@ -14,7 +14,7 @@ export class RegistroProfesorComponent implements OnInit {
 
   //variables
   nuevoRegistro: Profesor = null;
-  profesor:FormGroup;
+  profesor: FormGroup;
   submitted = false;
   mostrarMensaje = '';
 
@@ -25,50 +25,53 @@ export class RegistroProfesorComponent implements OnInit {
 
     //creamos las condiciones de los campos del formulario de registro
     this.profesor = this.formBuilder.group({
-      nickProfesor:['',Validators.required],
-      contrasenyaProfesor:['',Validators.required],
-      confirmarContrasenyaProfesor:['',Validators.required],
-      nombreProfesor:['',Validators.required],
-      apellidoProfesor:['',Validators.required],
-      correoProfesor:['',Validators.required, Validators.email],
-      centroProfesor:['', Validators.required]
-    },{
-      validator: validarContrasenya('contrasenyaProfesor','confirmarContrasenyaProfesor')
-    })
+      nickProfesor: ['', Validators.required],
+      contrasenyaProfesor: ['', [Validators.required,Validators.minLength(6), Validators.pattern('((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20})')]],
+      confirmarContrasenyaProfesor: ['', Validators.required],
+      nombreProfesor: ['', Validators.required],
+      apellidoProfesor: ['', Validators.required],
+      correoProfesor: ['', [Validators.required, Validators.email]],
+      centroProfesor: ['', Validators.required]
+    }, {
+      validator: validarContrasenya('contrasenyaProfesor', 'confirmarContrasenyaProfesor')
+    });
 
   }
 
   //funcion que se ejecuta al enviar el formulario
-  onFormSubmit(itemForm: any): void{
+  onFormSubmit(): void {
+
+    // console.log(itemForm);
+
+    console.log(this.profesor);
+
 
     //guardamos los datos del nuevo usuario en un registro nuevo
-    this.nuevoRegistro = new Profesor(itemForm.controls.nickProfesor.value,
-                                      itemForm.controls.contrasenyaProfesor.value,
-                                      itemForm.controls.confirmarContrasenyaProfesor.value,
-                                      itemForm.controls.nombreProfesor.value,
-                                      itemForm.controls.apellidoProfesor.value,
-                                      itemForm.controls.correoProfesor.value,
-                                      itemForm.controls.centroProfesor.value,);
-
-
+    this.nuevoRegistro = new Profesor(this.profesor.controls.nickProfesor.value,
+      this.profesor.controls.contrasenyaProfesor.value,
+      this.profesor.controls.confirmarContrasenyaProfesor.value,
+      this.profesor.controls.nombreProfesor.value,
+      this.profesor.controls.apellidoProfesor.value,
+      this.profesor.controls.correoProfesor.value,
+      this.profesor.controls.centroProfesor.value);
 
   }
 
-  get controlFormulario(){
+  get controlFormulario() {
     return this.profesor.controls;
   }
 
   //funcion que se ejecuta en el html para mandar los datos
-  enviarDatos(){
+  enviarDatos() {
     this.submitted = true;
 
     //si no se cumplen las condiciones
-    if (this.profesor.invalid){
+    if (this.profesor.invalid) {
       return;
     }
 
-  //si todos los datos y campos son correctos se muestra la ventana emergente
-  Swal.fire('Los datos introducidos son corectos');
+    //si todos los datos y campos son correctos se muestra la ventana emergente
+    Swal.fire('Los datos introducidos son corectos');
   }
 
 
