72ce39811f467bc5287d457a0ebb511f60124fab
SusannaMontero <sumonterog@gmail.com>
2021-02-16 19:48:59 +0100

    resolviendo registro

--- archivosPhp/comprovacioRegistre.php
+++ archivosPhp/comprovacioRegistre.php
@@ -11,18 +11,21 @@ $conexion = conexion();
 
 // realizamos la query a la BD
 $registros = mysqli_query($conexion, "SELECT * FROM profesores WHERE nickProfesor='$_GET[nickProfesor]");
-
+console_log("campo1");
 $datos= [];
 
 // si el profesor existe obtiene datos y los guarda en un array
 if ($resultado = mysqli_fetch_array($registros)) {
-
+  console_log("campo2");
   $datos[] = $resultado;
+}else{
+  mysqli_query($conexion, "INSERT * FROM profesores WHERE nickProfesor='$_GET[nickProfesor]");
+  console_log("no existe");
 }
 
 // genera el json con los datos obtenidos
 $json = json_encode($datos);
-
+console_log("campo3");
 // muestra el json generado
 echo $json;
 
