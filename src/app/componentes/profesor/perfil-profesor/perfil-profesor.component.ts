eb02d9968557284175096be663a7b50b6a223ef1
SusannaMontero <sumonterog@gmail.com>
2021-02-25 19:44:15 +0100

    ......

--- src/app/componentes/profesor/perfil-profesor/perfil-profesor.component.ts
+++ src/app/componentes/profesor/perfil-profesor/perfil-profesor.component.ts
@@ -1,4 +1,7 @@
+import { ProfesorService } from './../../../services/profesor.service';
 import { Component, OnInit } from '@angular/core';
+import { Profesor } from '../../../models/profesor.model';
+
 
 @Component({
   selector: 'app-perfil-profesor',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilProfesorComponent implements OnInit {
 
-  constructor() { }
+profesor: Profesor = null;
+profesorArray: Profesor[] = [];
+response: string = null;
+
+
+  constructor(private swService: ProfesorService) { }
 
   ngOnInit(): void {
+// usamos el servicio para pedir todos los datos en lugar de crearlos aquí
+  //  this.profesorArray = this.swService.getAllDades();
+
+    // carreguem les dades que tinguem guardant previament en memoria del navegador
+   // const profesore = localStorage.getItem('profesor');
+    //this.profesor = JSON.parse(profesore);
+
+
+
+    /*this.swService.getPerfilProfesor().subscribe(
+      (resp: Profesor[])->{
+        this.profesor = resp,
+        console.log(this.profesor),
+      },
+      (error: any) -> {
+        console.log(error);
+      }
+    );*/
+
+    this.profesorArray.push(new Profesor('NickProfesor', 'NombreProfesor', 'ApellidoProfesor', 'correo', 'contrasenyaProfesor', 'confirmarContrasenyaProfesor', 'centroProfesor'));
   }
+
 
 }
