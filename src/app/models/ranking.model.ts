89b4c02fcd387eef120045ace0897f442f81130c
NoemiSusa <noemisusa@gmail.com>
2021-04-07 20:09:16 +0200

    creacion modelo Rkg i create table Rankings

---
+++ src/app/models/ranking.model.ts
@@ -0,0 +1,11 @@
+export class Ranking {
+
+  constructor(
+    public nombreRanking?: string,
+    public nickProfesorRK?: string,
+    public fechaInicio?: Date,
+    public codigoAcceso?: string,
+    public fechaFinal?: Date
+  ) { }
+
+}
