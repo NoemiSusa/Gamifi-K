ac67d02f751338c024246e2e8ed65dfc3d55c73e
DAW2 <57674244+cacike69@users.noreply.github.com>
2021-04-07 20:10:41 +0200

    Reactive From ranking in progress

--- src/app/componentes/profesor/generar-ranking/generar-ranking.component.ts
+++ src/app/componentes/profesor/generar-ranking/generar-ranking.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Ranking } from 'src/app/models/ranking,model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-generar-ranking',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./generar-ranking.component.css']
 })
 export class GenerarRankingComponent implements OnInit {
+  //variable de tipo ranking para rellenar los datos del formulario
+  nuevoRanking : Ranking = null;
 
-  constructor() { }
+  rankingProfesor:FormGroup;
 
-  ngOnInit(): void {
+
+  submitted=false;
+  constructor(
+     private formBuilder: FormBuilder,
+    ) {
+
   }
 
+  ngOnInit(): void {
+this.rankingProfesor = this.formBuilder.group({
+  nombreRanking:['',[Validators.required,Validators.minLength(2)]],
+  fechaInicio:['',[Validators.required]]
+
+
+})
+
+  }
+
+    //sirve para ejecutar el control del formulario en el html
+    get controlFormulario() {
+      return this.rankingProfesor.controls;
+    }
+
+  funcionRanking(){
+this.nuevoRanking = new Ranking(this.rankingProfesor.controls.nombreRanking.value,
+  this.rankingProfesor.controls.fechaInicio.value,
+
+
+  );
+  this.nuevoRanking.nickProfesorRK=environment.vsesion;
+console.log(this.nuevoRanking);
+
+  }
+
+
+
 }
+
---
+++ src/app/models/ranking,model.ts
@@ -0,0 +1,13 @@
+export class Ranking {
+
+  constructor(
+    public nombreRanking?: string,
+    public nickProfesorRK?: string,
+    public fechaInicio?: Date,
+    public codigoAcceso?: string,
+    public fechaFinal?: Date
+  ) { }
+
+}
+
+
