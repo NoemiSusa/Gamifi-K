b81b4e31e21ec60f0ddfc0d59213cba2786632c3
NoemiSusa <72407457+NoemiSusa@users.noreply.github.com>
2021-04-26 09:50:04 +0200

    editar ranking
    solventamos pequeños problemas al querer mostrar el componente de editar ranking
    hace falta crear el php editar Rkg. con:
    - listado de alumnos y puntuación de una tarea concreta del ranking y professor seleccionados para poder editar la punuación de la tarea si es necesaria

--- archivosPhp/mostrarRankingOrdenadoPuntuacion.php
+++ archivosPhp/mostrarRankingOrdenadoPuntuacion.php
@@ -18,7 +18,7 @@ $conexion = conexion();
 
 
 //query que mostra les dades de l'alumne i la puntuació en 2 columnes i una fila per alumne ordenat de major a menor puntuació
-$query = "SELECT CONCAT( al.nickAlumno, CONCAT( ' ', CONCAT( al.apellidosAlumno, CONCAT( ' ', CONCAT( al.nombreAlumno, ' ' ) ) ) ) ) AS Alumno, SUM( t.puntuacion ) AS Puntuación
+$query = "SELECT CONCAT( al.nickAlumno, CONCAT( ' ', CONCAT( al.apellidosAlumno, CONCAT( ' ', CONCAT( al.nombreAlumno, ' ' ) ) ) ) ) AS Alumno, SUM( t.puntuacion ) AS Puntuacion
           FROM alumno al, rankings r, tareas t
           WHERE r.idRanking = '".$params->idRanking."'
           AND t.idRankingTarea = r.idRanking
