7db51bd8ab525156d26629586270d68a40a15556
DAW2 <57674244+cacike69@users.noreply.github.com>
2021-04-30 21:00:22 +0200

    Delete Ranking works
    Da errores de consola pero funciona

--- src/app/componentes/profesor/listar-rankings/listar-rankings.component.ts
+++ src/app/componentes/profesor/listar-rankings/listar-rankings.component.ts
@@ -4,6 +4,7 @@ import { ProfesorService } from './../../../services/profesor.service';
 import { Router } from '@angular/router';
 import { Ranking } from 'src/app/models/ranking.model';
 import { Respuesta } from 'src/app/models/respuesta.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-listar-rankings',
@@ -12,17 +13,17 @@ import { Respuesta } from 'src/app/models/respuesta.model';
 })
 export class ListarRankingsComponent implements OnInit {
 
-// Variables
-//rankingsArray: Ranking[] = null;
-respuestaR: Ranking;
-resp;
+  // Variables
+  //rankingsArray: Ranking[] = null;
+  respuestaR: Ranking;
+  resp;
+  respuesta: Respuesta;
+  nombreRanking: Ranking;
+  rankingSelected: Ranking;
+  idR: number = null;
 
-nombreRanking: Ranking;
-rankingSelected: Ranking;
-idR: number = null;
-
-sesion: string = environment.vsesion;
-idRanking: number;
+  sesion: string = environment.vsesion;
+  idRanking: number;
 
   constructor(
     // Creamos el objeto ranking del ServiceProfesor
@@ -32,13 +33,13 @@ idRanking: number;
 
   ngOnInit(): void {
 
-// Usamos el servicio par pedir todos los campos del ranking y poder listarlo
+    // Usamos el servicio par pedir todos los campos del ranking y poder listarlo
     this.listarRankings.pedirListadoRankings(this.sesion).subscribe(
       (resp: any) => {
         this.respuestaR = resp;
 
         console.log(this.respuestaR);
-       // console.log(this.respuestaR[0]['nombreRanking']);
+        // console.log(this.respuestaR[0]['nombreRanking']);
 
 
       },
@@ -48,13 +49,63 @@ idRanking: number;
     )
   }
 
-// Función que se ejecuta con click en el ranking que queremos seleccionar para editar
-    selectRanking(nombreRanking: Ranking):void  {
-     // this.rankingSelected = nombreRanking;
-      console.log(nombreRanking);
-      // especifico el campo del objeto que quiero guardar como variable global
-      environment.idRanking  = nombreRanking['idRanking'];
-      //environment.idRanking = this.idRanking;
-      console.log(environment.idRanking);
+  // Función que se ejecuta con click en el ranking que queremos seleccionar para editar
+  selectRanking(nombreRanking: Ranking): void {
+    // this.rankingSelected = nombreRanking;
+    console.log(nombreRanking);
+    // especifico el campo del objeto que quiero guardar como variable global
+    environment.idRanking = nombreRanking['idRanking'];
+    //environment.idRanking = this.idRanking;
+    console.log(environment.idRanking);
+
+  }
+
+  eliminarRanking(nombreRanking : Ranking): void {
+    environment.idRanking = nombreRanking['idRanking'];
+
+    console.log(environment.idRanking+"  El id del ranking line 64: ");
+    Swal.fire({
+      title: 'Estas seguro que quieres borrar el ranking?',
+      text: "No pueder revertir la decision",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        console.log(result);
+        this.listarRankings.eliminarRanking(environment.idRanking).subscribe(
+          (resp: any) => {
+            this.respuestaR = resp;
+            console.log(this.respuestaR);
+            Swal.fire(
+              'Borrado!',
+              this.respuesta.msg,
+              'success'
+            )
+
+            // console.log(this.respuestaR[0]['nombreRanking']);
+          },
+          (error: any) => {
+            console.log(error);
+          }
+        )
+      }
+
+      // Swal.fire(
+      //   'Deleted!',
+      //   'Your file has been deleted.',
+      //   'success'
+      // )
     }
+    )
+    // console.log(nombreRanking);
+    // environment.idRanking  = nombreRanking['idRanking'];
+
+
+  }
+
+
 }
+
